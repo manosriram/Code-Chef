33f7422d9f1926fe27619ed199b5e6d7b7aeff61
Mano Sriram <mano.sriram0@gmail.com>
2019-11-20 00:46:24 +0530

    Indent.

--- NOV19/CAMC.cpp
+++ NOV19/CAMC.cpp
@@ -17,23 +17,23 @@ int main() {
 
         for (int t=1;t<=n;t++) {
             cin >> temp;
-            v[t%m].push_back(temp);            
-        }
-        
-        for (int t=1;t<=n;t++) {
-        pair<vector<int>::iterator, vector<int>::iterator> p1 = minmax_element(v[t%m].begin(), v[t%m].end());
-        
-        p[t%m].first = *p1.first;
-        p[t%m].second= *p1.second;
+            v[t%m].push_back(temp);
         }
 
-        int diff1, diff2, diff; 
+        for (int t=1;t<=n;t++) {
+            pair<vector<int>::iterator, vector<int>::iterator> p1 = minmax_element(v[t%m].begin(), v[t%m].end());
+
+            p[t%m].first = *p1.first;
+            p[t%m].second= *p1.second;
+        }
+
+        int diff1, diff2, diff;
 
         for (int t=1;t<=n;++t) {
             for (int j=t+1;j<=n;++j) {
-diff1 = min(abs(p[t%m].first - p[j%m].first), abs(p[t%m].first - p[j%m].second));
-diff2 = min(abs(p[t%m].second - p[j%m].first), abs(p[t%m].second - p[j%m].second));
-diff = min(diff1, diff2);
+                diff1 = min(abs(p[t%m].first - p[j%m].first), abs(p[t%m].first - p[j%m].second));
+                diff2 = min(abs(p[t%m].second - p[j%m].first), abs(p[t%m].second - p[j%m].second));
+                diff = min(diff1, diff2);
             }
         }
         cout << diff << '\n';
