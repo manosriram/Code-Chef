5e6d61e6a75c9d7a2aac625493d8168fd754424f
manosriram <mano.sriram0@gmail.com>
2020-03-31 00:00:13 +0530

    DIGJMP

---
+++ Practice/Graphs/DIGJUMP.cpp
@@ -0,0 +1,38 @@
+// g++ -std=c++17 DIGJUMP.cpp -o a.out && ./a.out < in
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <set>
+#include <time.h>
+#include "unordered_map"
+using namespace std;
+#define MAX INT_MAX
+#define MIN INT_MIN
+#define INF 1e9
+#define pb push_back
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef long long ll;
+
+int main() {
+    cin.sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    clock_t tStart = clock();
+    freopen("out", "w", stdout);
+    freopen("err", "w", stderr);
+#endif
+    string s;
+
+
+#ifndef ONLINE_JUDGE
+    cout << "Time Taken: " << (double)(clock() - tStart)/CLOCKS_PER_SEC << endl;
+#endif
+
+}
